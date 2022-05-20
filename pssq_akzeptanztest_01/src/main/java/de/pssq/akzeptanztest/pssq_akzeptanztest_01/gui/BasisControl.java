62721aad01a19cc9d8e1f151cd2b2da3b20e57c6
pwegner <wegner@ial.ruhr>
2022-05-20 19:13:19 +0200

    better exceptionhandling

--- pssq_akzeptanztest_01/src/main/java/de/pssq/akzeptanztest/pssq_akzeptanztest_01/gui/BasisControl.java
+++ pssq_akzeptanztest_01/src/main/java/de/pssq/akzeptanztest/pssq_akzeptanztest_01/gui/BasisControl.java
@@ -49,31 +49,43 @@ public class BasisControl implements Initializable {
 
 	@FXML
 	public void addArtikel() {
-		if (!this.artikelIDField.getText().isEmpty() && !this.lagerbestandField.getText().isEmpty()
-				&& !this.bezeichnungBox.getValue().isEmpty() && !this.preisField.getText().isEmpty()) {
-			Artikel artikel = new Artikel(Integer.parseInt(this.artikelIDField.getText()),
-					this.bezeichnungBox.getSelectionModel().selectedItemProperty().getValue(),
-					Integer.parseInt(this.lagerbestandField.getText()), Double.parseDouble(this.preisField.getText()));
-			this.basisModel.fuegeArtikelHinzu(artikel);
-		} else {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setTitle("Eintrag hinzufügen");
-			alert.setHeaderText("Daten unvollständig.");
-			alert.setContentText("Bitte Daten vervollständigen.");
-			alert.show();
-		}
+		this.addArtikel(
+				this.artikelIDField.getText(), 
+				this.bezeichnungBox.getValue(), 
+				this.lagerbestandField.getText(), 
+				this.preisField.getText()
+		);
+		
 		this.artikelIDField.setText("");
 		this.bezeichnungBox.getSelectionModel().clearSelection();
 		this.bezeichnungBox.setValue("");
 		this.lagerbestandField.setText("");
 		this.preisField.setText("");
-
+		
 		this.refreshArtikel();
+	}
+	
+	public void addArtikel(String artikelIDField, String bezeichnungBoxField, String lagerbestandField, String preisField) {
+		try {
+			int artikelID = Integer.parseInt(artikelIDField);
+			String bezeichnung = bezeichnungBoxField;
+			int lagerbestand = Integer.parseInt(lagerbestandField);
+			double preis = Double.parseDouble(preisField);
+			
+			if (artikelID < 0 | preis < 0) {
+				this.showAlert();
+			} else {
+				Artikel artikel = new Artikel(artikelID, bezeichnung, lagerbestand, preis);
+				this.basisModel.fuegeArtikelHinzu(artikel);				
+			}
+		} catch (Exception e) {
+			this.showAlert();
+		}
 	}
 
 	@FXML
 	public void btnLoescheArtikel() {
-		if (this.tableView.getSelectionModel().getSelectedItem() != null) {
+		if(this.tableView.getSelectionModel().getSelectedItem() != null) {
 			Artikel martierteArtikelReihe = this.tableView.getSelectionModel().getSelectedItem();
 			this.basisModel.loescheArtikel(martierteArtikelReihe);
 		}
@@ -91,18 +103,29 @@ public class BasisControl implements Initializable {
 		for (Artikel artikel : this.getArtikelListe()) {
 			double preis = artikel.getPreis();
 			if (this.mitMwSt) {
-				artikel.setPreis(preis / 1.19);
 				this.btnMwSt.setText("Mit MwSt");
 			} else {
-				artikel.setPreis(preis * 1.19);
 				this.btnMwSt.setText("Ohne MwSt");
 			}
+			artikel.setPreis(this.berechneMwSt(preis));
 			artikelListe.add(artikel);
 		}
 		this.tableView.getItems().clear();
 		this.basisModel.setArtikelListe(artikelListe);
 		this.mitMwSt = !this.mitMwSt;
 		this.refreshArtikel();
+	}
+	
+	public double berechneMwSt(double preis) {
+		if (this.mitMwSt) {
+			return preis / 1.19;
+		} else {
+			return preis * 1.19;
+		}
+	}
+	
+	public double mitMwSt(double preis) {
+		return preis * 1.19;
 	}
 
 	public ObservableList<Artikel> getArtikelListe() {
@@ -119,12 +142,20 @@ public class BasisControl implements Initializable {
 		this.tableView.setItems(this.getArtikelListe());
 	}
 
+	public void showAlert() {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Eintrag hinzufügen");
+		alert.setHeaderText("Daten unvollständig.");
+		alert.setContentText("Bitte Daten vervollständigen.");
+		alert.show();
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		artikelIDColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Integer>("artikelID"));
-		bezeichnungColumn.setCellValueFactory(new PropertyValueFactory<Artikel, String>("bezeichnung"));
-		lagerbestandColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Integer>("lagerbestand"));
-		preisColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Double>("preis"));
+		this.artikelIDColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Integer>("artikelID"));
+		this.bezeichnungColumn.setCellValueFactory(new PropertyValueFactory<Artikel, String>("bezeichnung"));
+		this.lagerbestandColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Integer>("lagerbestand"));
+		this.preisColumn.setCellValueFactory(new PropertyValueFactory<Artikel, Double>("preis"));
 		this.refreshArtikel();
 	}
 }
