480bee4458c793473c4ec31849b0e706c349eac6
pwegner <wegner@ial.ruhr>
2022-05-21 14:03:42 +0200

    jbehave 03

---
+++ pssq_akzeptanztest_03/src/main/java/de/pssq/akzeptanztest/pssq_akzeptanztest_03/Main.java
@@ -0,0 +1,36 @@
+package de.pssq.akzeptanztest.pssq_akzeptanztest_03;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class Main extends Application {
+
+	@SuppressWarnings("exports")
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("ArtikelApp");
+			FXMLLoader loader = new FXMLLoader();
+			System.out.println("LOCATION: " + getClass().getResource("gui/BasisView.fxml"));
+			loader.setLocation(getClass().getResource("gui/BasisView.fxml"));
+			GridPane root = loader.load();
+			Scene scene = new Scene(root, 750, 490);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
