b5b9d78a45985c2c50617a6052f4e75d3690bc4c
pwegner <wegner@ial.ruhr>
2022-05-24 14:05:52 +0200

    final commit

--- pssq_akzeptanztest_03/src/main/java/de/pssq/akzeptanztest/pssq_akzeptanztest_03/HelloWorld.java
+++ pssq_akzeptanztest_03/src/main/java/de/pssq/akzeptanztest/pssq_akzeptanztest_03/HelloWorld.java
@@ -3,9 +3,7 @@ package de.pssq.akzeptanztest.pssq_akzeptanztest_03;
 public class HelloWorld {
 
 	public String sayHello(String name) {
-		
 		System.out.println("sayHello called with name : " + name);
-
 		return "Hello " + name;
 	}
 
